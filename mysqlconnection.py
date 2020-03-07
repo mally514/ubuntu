b96a07f6693bcc674a2ef71f837252ef4ea0a914
= <=>
2020-03-07 00:15:51 -0400

    password

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -5,7 +5,7 @@ class MySQLConnection:
         connection = pymysql.connect(
             host = 'localhost',
             user = 'root', # change the user and password as needed
-            password = '', 
+            password = 'root', 
             db = 'crud',
             charset = 'utf8mb4',
             cursorclass = pymysql.cursors.DictCursor,
